c417c4ec79f7749e51df21aa5ddf9391b3673b93
Clebson Santos <clebsonsantosmtt@gmail.com>
2021-04-07 21:19:00 -0300

    Add NavBar

--- src/Components/Aside/index.tsx
+++ src/Components/Aside/index.tsx
@@ -1,10 +1,49 @@
 import React from 'react';
+import logoImg from '../../assets/logo.svg'
 
-import { Container } from './styles'
+
+import {
+  MdDashboard,
+  MdArrowDownward,
+  MdArrowUpward,
+  MdExitToApp
+
+ } from 'react-icons/md' 
+
+import { 
+  Container, 
+  Header, 
+  LogoImg, 
+  MenuContainer, 
+  MenuItemLink, 
+  Title
+} from './styles'
 const Aside: React.FC = () =>{
   return (
     <Container>
-    <h1>Aside</h1>
+      <Header>
+        <LogoImg src={logoImg} alt="Logo minha carteira"/>
+        <Title>Minha Carteira</Title>
+      </Header>
+
+      <MenuContainer>
+          <MenuItemLink href="#">
+            <MdDashboard/>
+            Dashboard
+          </MenuItemLink>
+          <MenuItemLink href="#">
+            <MdArrowUpward/>
+            Entradas
+          </MenuItemLink>
+          <MenuItemLink href="#">
+            <MdArrowDownward/>
+            Saídas
+          </MenuItemLink>
+          <MenuItemLink href="#">
+            <MdExitToApp/>
+            Sair
+          </MenuItemLink>
+      </MenuContainer>
     </Container>
   );
 }
