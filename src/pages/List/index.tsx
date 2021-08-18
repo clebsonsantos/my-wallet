b018322126366795bd96b1bc0a7106060323eba0
Clebson Santos <clebsonsantosmtt@gmail.com>
2021-08-18 14:52:48 -0300

    Filtros de recorrentes e eventuais

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -3,178 +3,101 @@ import ContentHeader from '../../Components/ContentHeader'
 import SelectInput from '../../Components/SelectInput'
 import HistoryFinanceCard from '../../Components/HistoryFinanceCard'
 
-import {Container, Content } from './style'
+import {Container, Content, Filters } from './style'
 const List: React.FC = () =>{
-  const options = [
-    {value: 'Rodrigo', label: 'Rodrigo'},
-    { value: 'Maria', label: 'Maria'},
-    { value: 'Carlos', label: 'Carlos'}
+  const months = [
+    {value: 7, label: 'Julho'},
+    { value: 8, label: 'Agosto'},
+    { value: 9, label: 'Setembro'}
 
   ]
+  const years = [
+    {value: 2018, label: 2018},
+    { value: 2019, label: 2019},
+    { value: 2020, label: 2020}
 
+  ]
+  function handleFrequencyClick(information: string){
+    return alert(information)
+  }
   return (
     <>
-    <Container>
-        <ContentHeader title='Lista' lineColor="#c91919">
-          <SelectInput options={options}/>
+      <Container>
+        <ContentHeader title="Lista" lineColor="#c91919">
+          <SelectInput options={months} />
+          <SelectInput options={years} />
         </ContentHeader>
+        <Filters>
+          <button
+            type="button"
+            className={`
+                    tag-filter 
+                    tag-filter-recurrent
+                    
+                    `}
+            // ${frequencyFilterSelected.includes('recorrente') && 'tag-actived'}
+            onClick={() => handleFrequencyClick('recorrente')}
+          >
+            Recorrentes
+          </button>
+
+          <button
+            type="button"
+            className={`
+                    tag-filter 
+                    tag-filter-eventual
+                    `}
+            // ${frequencyFilterSelected.includes('eventual') && 'tag-actived'}
+            onClick={() => handleFrequencyClick('eventual')}
+          >
+            Eventuais
+          </button>
+        </Filters>
         <Content>
           <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de NetFlix"
-          subtitle="17/08/2021"
-          amount="R$ 27,50"
-
+            tagColor="#e44c4e"
+            title="Conta de NetFlix"
+            subtitle="17/08/2021"
+            amount="R$ 27,50"
           />
           <HistoryFinanceCard
-          tagColor="#e44ddd"
-          title="Conta de Luz"
-          subtitle="17/08/2021"
-          amount="R$ 150,50"
-
-          />
-           <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de NetFlix"
-          subtitle="17/08/2021"
-          amount="R$ 27,50"
-
+            tagColor="#e44ddd"
+            title="Conta de Luz"
+            subtitle="17/08/2021"
+            amount="R$ 150,50"
           />
           <HistoryFinanceCard
-          tagColor="#e44ddd"
-          title="Conta de Luz"
-          subtitle="17/08/2021"
-          amount="R$ 150,50"
-
-          />
-           <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de NetFlix"
-          subtitle="17/08/2021"
-          amount="R$ 27,50"
-
+            tagColor="#e44c4e"
+            title="Conta de NetFlix"
+            subtitle="17/08/2021"
+            amount="R$ 27,50"
           />
           <HistoryFinanceCard
-          tagColor="#e44ddd"
-          title="Conta de Luz"
-          subtitle="17/08/2021"
-          amount="R$ 150,50"
-
-          />
-           <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de NetFlix"
-          subtitle="17/08/2021"
-          amount="R$ 27,50"
-
+            tagColor="#e44ddd"
+            title="Conta de Luz"
+            subtitle="17/08/2021"
+            amount="R$ 150,50"
           />
           <HistoryFinanceCard
-          tagColor="#e44ddd"
-          title="Conta de Luz"
-          subtitle="17/08/2021"
-          amount="R$ 150,50"
-
-          />
-           <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de NetFlix"
-          subtitle="17/08/2021"
-          amount="R$ 27,50"
-
+            tagColor="#e44c4e"
+            title="Conta de NetFlix"
+            subtitle="17/08/2021"
+            amount="R$ 27,50"
           />
           <HistoryFinanceCard
-          tagColor="#e44ddd"
-          title="Conta de Luz"
-          subtitle="17/08/2021"
-          amount="R$ 150,50"
-
-          />
-           <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de NetFlix"
-          subtitle="17/08/2021"
-          amount="R$ 27,50"
-
+            tagColor="#e44ddd"
+            title="Conta de Luz"
+            subtitle="17/08/2021"
+            amount="R$ 150,50"
           />
           <HistoryFinanceCard
-          tagColor="#e44ddd"
-          title="Conta de Luz"
-          subtitle="17/08/2021"
-          amount="R$ 150,50"
-
-          />
-           <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de NetFlix"
-          subtitle="17/08/2021"
-          amount="R$ 27,50"
-
-          />
-          <HistoryFinanceCard
-          tagColor="#e44ddd"
-          title="Conta de Luz"
-          subtitle="17/08/2021"
-          amount="R$ 150,50"
-
-          />
-           <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de NetFlix"
-          subtitle="17/08/2021"
-          amount="R$ 27,50"
-
-          />
-          <HistoryFinanceCard
-          tagColor="#e44ddd"
-          title="Conta de Luz"
-          subtitle="17/08/2021"
-          amount="R$ 150,50"
-
-          />
-           <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de NetFlix"
-          subtitle="17/08/2021"
-          amount="R$ 27,50"
-
-          />
-          <HistoryFinanceCard
-          tagColor="#e44ddd"
-          title="Conta de Luz"
-          subtitle="17/08/2021"
-          amount="R$ 150,50"
-
-          />
-           <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de NetFlix"
-          subtitle="17/08/2021"
-          amount="R$ 27,50"
-
-          />
-          <HistoryFinanceCard
-          tagColor="#e44ddd"
-          title="Conta de Luz"
-          subtitle="17/08/2021"
-          amount="R$ 150,50"
-
-          />
-           <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de NetFlix"
-          subtitle="17/08/2021"
-          amount="R$ 27,50"
-
-          />
-          <HistoryFinanceCard
-          tagColor="#e44ddd"
-          title="Conta de Luz"
-          subtitle="17/08/2021"
-          amount="R$ 150,50"
-
+            tagColor="#e44c4e"
+            title="Conta de NetFlix"
+            subtitle="17/08/2021"
+            amount="R$ 27,50"
           />
         </Content>
-    </Container>
+      </Container>
     </>
   );
 }
